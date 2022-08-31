1808dc7fbce55337cd0a6a5ae0c0c557adc70f46
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-08-30 20:16:42 -0500

    thought model

---
+++ controllers/user-controller.js
@@ -0,0 +1,103 @@
+const res = require("express/lib/response");
+const { User, Thought } = require("../models");
+
+const userController = {
+  getAllUser(req, res) {
+    User.find({})
+      .populate({
+        path: "",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+  //to getONE user by Id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+  //update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidatora: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user was found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "No user was found with this id!" });
+        }
+      })
+      .then(() => {
+        res.json({ message: "User and thoughts no longer available! " });
+      })
+      .catch((err) => res.json(err));
+  },
+  //to add a friend
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $addToSet: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: " No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+  //remove a friend
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "No user found with this Id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
+module.exports = userController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,9 +1,46 @@
+//import
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = require("./Reaction");
 //thoughtText to have String, Required, Must be between 1 and 280 characters
-
-//createdAt to have Date, set default value to the current timestamp, use a getter method to format the timestamp on query
-
-//username to have a string and required
-
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: " Thought is required",
+      maxlength: 250,
+      minlength: 1,
+    },
+    //createdAt to have Date, set default value to the current timestamp, use a getter method to format the timestamp on query
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //using a getter method to format timestamp
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    //username to have a string and required
+    username: {
+      type: String,
+      requires: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 //reactions to have an array of nested documents created with the reactionSchema
 
 //create a virtual called reactionCount that retrieves the length of the thoughts reactions array field on query
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+model.exports = Thought;
