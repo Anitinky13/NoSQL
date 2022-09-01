48bd80df0d4b7c4b6821724c3ad49b576f021393
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-01 12:36:14 -0500

    Realized Reactions is not a model, so moved it to thoughts as said in challenge directions

--- models/Thought.js
+++ models/Thought.js
@@ -2,27 +2,65 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const ReactionSchema = require("./Reaction");
+// const ReactionSchema = require("./Reaction");
+const ReactionSchema = new Schema(
+  {
+    //schema only
+
+    //reactionId to use mongooses objectId data type and default value is set to a new objectId
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+
+    //reactionBody to have a string, required, and 280 character maximum
+    reactionBody: {
+      type: String,
+      required: "Enter your reaction",
+      minLength: 2,
+      maxlength: 280,
+    },
+
+    //username to have a string and required
+    username: {
+      type: String,
+      required: "Enter your username",
+    },
+    //createdAt to have a date, set default value to the current timestamp and use a getter method to format the timestamp on query
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtValidator) => dateFormat(createdAtValidator),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 //thoughtText to have String, Required, Must be between 1 and 280 characters
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
       required: " Thought is required",
+      trim: true,
       maxlength: 250,
-      minlength: 1,
+      minlength: 2,
     },
     //createdAt to have Date, set default value to the current timestamp, use a getter method to format the timestamp on query
     createdAt: {
       type: Date,
       default: Date.now,
       //using a getter method to format timestamp
-      get: (timestamp) => dateFormat(timestamp),
+      get: (createdAtValidator) => dateFormat(createdAtValidator),
     },
     //username to have a string and required
     username: {
       type: String,
-      requires: true,
+      required: "Enter username",
     },
     reactions: [ReactionSchema],
   },
@@ -37,7 +75,7 @@ const ThoughtSchema = new Schema(
 //reactions to have an array of nested documents created with the reactionSchema
 
 //create a virtual called reactionCount that retrieves the length of the thoughts reactions array field on query
-ThoughtSchema.virtual("reactionCount").get(function () {
+ThoughtSchema.virtual("reactionsCount").get(function () {
   return this.reactions.length;
 });
 
